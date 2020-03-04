86af53fd5fdb5237f99487ce676e091e77202abe
Angelina <angelina.blyth@bbc.co.uk>
2020-03-04 10:56:54 +0000

    Added Lab 9

---
+++ Labs/Lab 9/bookshopapp/src/main/scala/application/BookshopApp.scala
@@ -0,0 +1,14 @@
+package application
+
+import store._
+
+object BookshopApp extends App {
+  println("Welcome to the Bookshop\n")
+
+  Bookshop.printDetails()
+
+  println(s"store.Book: ${Bookshop.book}")
+  println("\nCalculating the store.Sales Discount price")
+  Bookshop.book.setSaleDiscount(10.0)
+  println(s"Sale price of book: ${Bookshop.book.calculateSalePrice()}")
+}
